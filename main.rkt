031ac98b97fccb4bbd9ad8fce4f444b305121ff8
Anurag muthyam <amuthyam@rccl.com>
2025-10-12 18:20:49 -0400

    fixed main.rkt

--- main.rkt
+++ main.rkt
@@ -1,10 +1,6 @@
 #lang racket
 
 (require "dev/math/combinatorics/factorial.rkt")
-(require "dev/math/combinatorics/anu-conjecture.rkt")
-(require "dev/math/combinatorics/range.rkt")
 
 (provide
-  (all-from-out "dev/math/combinatorics/factorial.rkt")
-  (all-from-out "dev/math/combinatorics/anu-conjecture.rkt")
-  (all-from-out "dev/math/combinatorics/range.rkt"))
+  (all-from-out "dev/math/combinatorics/factorial.rkt"))
